4253fc785619da4186a794b398751b3d249191bc
ceodspspectrum <ceo.dspspectrum@gmail.com>
2018-10-30 13:00:19 -0600

    Create Matrix_inv_np.py

---
+++ master/Matrix_inv_np.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import timeit
+import pandas as pd
+N=20
+i=1
+s=2
+num_repeats=10
+res=pd.DataFrame(columns=('N','Time'))
+for j in range(0,N):
+
+    A = np.random.rand(i, i).astype(np.float32)
+    #B = np.random.rand(i, i).astype(np.float32)
+
+    timer = timeit.Timer("np.linalg.inv(A)", "import numpy as np; from __main__ import A")
+    numpy_times_list = timer.repeat(num_repeats, 1)
+    times=np.mean(numpy_times_list)
+    res.loc[j]=[i,times]
+    res.to_csv('Result_mkl_inv_CV_base'+str(s)+'.csv',index=False)
+    i=i*s
